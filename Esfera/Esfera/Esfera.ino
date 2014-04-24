a0d6a8b4a1ff9e3c39b831d0700241710fb1b596
Escuchimi <jbarry10@gmail.com>
2014-04-24 15:13:40 +0200

    Ended calculate_velocity
    calculate_velocity function ended and running.

--- Esfera/Esfera/Esfera.ino
+++ Esfera/Esfera/Esfera.ino
@@ -1,11 +1,17 @@
 /* Minimum_Source*/
-#define TIMEOUT 500
+#define TIMEOUT                 500
+#define CALCULATE_VELOCITY_RATE 50
+#define CALCULATE_BATTERY_RATE  1000
+
 
 Dynamixel     Dxl(1);
 short         last_position;
-unsigned long last_time;
-unsigned long entrance_time;
+unsigned long absolut_last_time;
+unsigned long blind_zone_entrance_time;
 unsigned long timeout;
+unsigned long battery_entrance_time;
+unsigned long velocity_entrance_time;
+unsigned long time;
 float         velocity; 
 boolean       blind_zone;
 boolean       exit_edge;
@@ -18,36 +24,38 @@ void setup() {
   
  Dxl.begin(1);
  Dxl.writeWord(1, 8, 0);
- last_position = Dxl.readWord(1,36);
- last_time     = millis();
- velocity      = 0.0f;
- blind_zone    = false;
- exit_edge     = false;
- Dxl.writeWord(1, 32, 1200);
+ last_position          = Dxl.readWord(1,36);
+ absolut_last_time      = millis();
+ velocity_entrance_time = millis();
+ velocity               = 0.0f;
+ blind_zone             = false;
+ exit_edge              = false;
+ Dxl.writeWord(1, 32, 1023);
 }
 
 void loop() {
-
- 
-  calculate_velocity();
-  SerialUSB.println(velocity);
-  delay(50);
+  SerialUSB.println(millis()-time);
   
-  //SerialUSB.println(Dxl.readWord(1,38));
-  //if(Dxl.readWord(1,36) == 1023) SerialUSB.print("EYY");
-  // put your main code here, to run repeatedly: 
+  time = millis();
+  if(Serial3.available() 3
+  if(time - velocity_entrance_time >= CALCULATE_VELOCITY_RATE) calculate_velocity(); 
+
+  
+  
+ 
 }
 
 void calculate_velocity()
 {
-  short position = Dxl.readWord(1,36);
-  short step;
-  unsigned long time = millis();
-  step = position-last_position;
+  short position     = Dxl.readWord(1,36);
+  short step         = position-last_position;
+  
+  //time = millis();
+
   if(!blind_zone && (position == 1023 || position == 0 || (abs(step) > 150 && (position < 380 && position > 340))))
   {
     blind_zone    = true;
-    entrance_time = time;
+    blind_zone_entrance_time = time;
     timeout       = abs((293.0f/velocity)*204.6);
     exit_edge     = true;
   }
@@ -57,26 +65,18 @@ void calculate_velocity()
   {
     case true:
     {  float aux_time = (293/velocity)*204.6;
-      if((time - entrance_time) > timeout + TIMEOUT) velocity = 0.0f; 
-      else if((time - entrance_time) > aux_time &&  aux_time > 0 ) velocity = 293*204.6/float(time-entrance_time);
-      else if((time - entrance_time) > abs(aux_time))            {  velocity = -1*293*204.6/float(time-entrance_time);SerialUSB.print(time-entrance_time);}
+      if((time - blind_zone_entrance_time) > timeout + TIMEOUT) velocity = 0.0f; 
+      else if((time - blind_zone_entrance_time) > aux_time &&  aux_time > 0 ) velocity = 293*204.6/float(time-blind_zone_entrance_time);
+      else if((time - blind_zone_entrance_time) > abs(aux_time))              velocity = -1*293*204.6/float(time-blind_zone_entrance_time);
      break;
     }
     
     case false:
     
-      if(!exit_edge) velocity  = 293.0f*(float(step)/float(time-last_time)); //293 factor byte/millis to degrees/sec
+      if(!exit_edge) velocity  = 293.0f*(float(step)/float(time-velocity_entrance_time)); //293 factor byte/millis to degrees/sec
       else           exit_edge = false;
       break;
   }
-//  SerialUSB.print("Step = ");
-//  SerialUSB.print(step);
-//  SerialUSB.print("  Time increment = ");
-//  SerialUSB.print(time-last_time);
-//  SerialUSB.print("
-  SerialUSB.print(blind_zone);
-  SerialUSB.print(" - ");
- // SerialUSB.println(position);
-  last_time = time;
+  velocity_entrance_time = time;
   last_position = position;
 }
