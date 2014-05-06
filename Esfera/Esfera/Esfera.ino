c1f64dc08b0804d0b72548d7c0d6f4421b18e36f
Escuchimi <jbarry10@gmail.com>
2014-05-06 18:23:17 +0200

    OCM communication trials
    Added “update_motors”
    Added macroscopic variables

--- Esfera/Esfera/Esfera.ino
+++ Esfera/Esfera/Esfera.ino
@@ -1,10 +1,20 @@
 /* Minimum_Source*/
+
+#include <Serial_OCM_APM.h>
+
 #define TIMEOUT                 500
-#define CALCULATE_VELOCITY_RATE 50
+#define SEND_DATA_RATE          50
 #define CALCULATE_BATTERY_RATE  1000
 
 
 Dynamixel     Dxl(1);
+word          drive;
+word          steer;
+byte_float    velocity; 
+char          alarms[2];
+
+word          last_drive;
+word          last_steer;
 short         last_position;
 unsigned long absolut_last_time;
 unsigned long blind_zone_entrance_time;
@@ -12,13 +22,15 @@ unsigned long timeout;
 unsigned long battery_entrance_time;
 unsigned long velocity_entrance_time;
 unsigned long time;
-float         velocity; 
+
 boolean       blind_zone;
 boolean       exit_edge;
 
 
 void calculate_velocity(void);
+void update_motors(void);
 
+Serial_OCM_APM communication;
 void setup() {
   // put your setup code here, to run once:
   
@@ -27,22 +39,43 @@ void setup() {
  last_position          = Dxl.readWord(1,36);
  absolut_last_time      = millis();
  velocity_entrance_time = millis();
- velocity               = 0.0f;
+ velocity.f             = 0.0f;
  blind_zone             = false;
  exit_edge              = false;
- Dxl.writeWord(1, 32, 1023);
+ drive                  = 0;
+ steer                  = 0;
+ last_drive             = 0;
+ last_steer             = 0;
+
+// Dxl.writeWord(1, 32, 1023);
 }
 
 void loop() {
-  SerialUSB.println(millis()-time);
+  //SerialUSB.println(millis()-time);
   
   time = millis();
-  if(Serial3.available() 3
-  if(time - velocity_entrance_time >= CALCULATE_VELOCITY_RATE) calculate_velocity(); 
-
   
   
- 
+  communication.receive_message();
+  if(time - velocity_entrance_time >= SEND_DATA_RATE){
+//    char content[6];
+//    byte_word angle;
+//    
+//    angle.w = Dxl.readByte( 1, 36);
+//    calculate_velocity();
+//    
+//    content[0] = velocity.s[0];
+//    content[1] = velocity.s[1];
+//    content[2] = velocity.s[2];
+//    content[3] = velocity.s[3];
+//    
+//    content[4] = angle.b[0];
+//    content[5] = angle.b[1];
+//    
+//    //SerialUSB.println(content);
+//    communication.send_message(GET_AB,content);
+  }
+  update_motors();
 }
 
 void calculate_velocity()
@@ -56,7 +89,7 @@ void calculate_velocity()
   {
     blind_zone    = true;
     blind_zone_entrance_time = time;
-    timeout       = abs((293.0f/velocity)*204.6);
+    timeout       = abs((293.0f/velocity.f)*204.6);
     exit_edge     = true;
   }
   else if(blind_zone  && position != 1023 && position != 0  && ( position < 100 || position > 923) && abs(step) < 150) blind_zone = false;
@@ -64,19 +97,35 @@ void calculate_velocity()
   switch(blind_zone)
   {
     case true:
-    {  float aux_time = (293/velocity)*204.6;
-      if((time - blind_zone_entrance_time) > timeout + TIMEOUT) velocity = 0.0f; 
-      else if((time - blind_zone_entrance_time) > aux_time &&  aux_time > 0 ) velocity = 293*204.6/float(time-blind_zone_entrance_time);
-      else if((time - blind_zone_entrance_time) > abs(aux_time))              velocity = -1*293*204.6/float(time-blind_zone_entrance_time);
-     break;
+    {  
+      float aux_time = (293/velocity.f)*204.6;
+      if((time - blind_zone_entrance_time) > timeout + TIMEOUT) velocity.f = 0.0f; 
+      else if((time - blind_zone_entrance_time) > aux_time &&  aux_time > 0 ) velocity.f = 293*204.6/float(time-blind_zone_entrance_time);
+      else if((time - blind_zone_entrance_time) > abs(aux_time))              velocity.f = -1*293*204.6/float(time-blind_zone_entrance_time);
+     
+      break;
     }
     
     case false:
     
-      if(!exit_edge) velocity  = 293.0f*(float(step)/float(time-velocity_entrance_time)); //293 factor byte/millis to degrees/sec
+      if(!exit_edge) velocity.f  = 293.0f*(float(step)/float(time-velocity_entrance_time)); //293 factor byte/millis to degrees/sec
       else           exit_edge = false;
       break;
   }
   velocity_entrance_time = time;
   last_position = position;
+}
+
+void update_motors()
+{
+  if(last_drive != drive)
+  {
+    Dxl.writeWord(1,32,drive);
+    last_drive = drive;
+  }
+    if(last_steer != steer)
+  {
+    Dxl.writeWord(1,30,steer);
+    last_steer = steer;
+  }
 }
